859dab5b3f5987a5ba5087ccdd868d97614bdd7e
Kensuke Shibata <kensukeshibata@gmail.com>
2020-01-28 01:46:46 +0900

    added extensions which you don't need to specify js and jsx

--- src/components/CardList/card-list.component.jsx
+++ src/components/CardList/card-list.component.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import styled from "@emotion/styled";
-import Card from "../Card/card.component.jsx";
+import React from 'react';
+import styled from '@emotion/styled';
+import Card from '../Card/card.component';
 
 const CartListContainer = styled.div({
-  width: "85vw",
-  margin: "0 auto",
-  display: "grid",
-  gridTemplateColumns: "1fr 1fr 1fr",
-  gridGap: "50px",
-  marginTop: "150px"
+  width: '85vw',
+  margin: '0 auto',
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr 1fr',
+  gridGap: '50px',
+  marginTop: '150px',
 });
 
 const CardList = props => (
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  //This is default for webpack if you don't specify entry and output
+  // This is default for webpack if you don't specify entry and output
   // entry: './src',
   // output: {
   //   filename: '[name].js',
@@ -11,7 +11,7 @@ module.exports = {
     port: 3300,
     watchContentBase: true,
     open: true,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   module: {
     rules: [
@@ -19,34 +19,40 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: "file-loader"
-          }
-        ]
+            loader: 'file-loader',
+          },
+        ],
       },
       {
         test: /\.svg$/,
-        use: ["@svgr/webpack", "url-loader"]
+        use: ['@svgr/webpack', 'url-loader'],
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader"
-          }
-        ]
-      }
-    ]
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./index.html"
-    })
-  ]
+      template: './index.html',
+    }),
+  ],
 };
