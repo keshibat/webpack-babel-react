f2cdda1ca3f67bf5f12db7b2493a9688da62b804
keshibat <kensukeshibata@gmail.com>
2020-01-27 20:11:25 +0900

    being able to diplay the view

--- src/components/Card/card.component.jsx
+++ src/components/Card/card.component.jsx
@@ -1,36 +1,36 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import React from "react";
+import styled from "@emotion/styled";
 
 const CardWrapper = styled.div({
-  display: 'flex',
-  flexDirection: 'column',
-  background: 'linear-gradient(90deg,rgb(77,40,180),rgba(50,145,255,0.8))',
-  border: '1px solid grey',
-  borderRadius: '10px',
-  padding: '15px',
-  cursor: 'pointer',
-  mozOsxFontSmoothing: 'grayscale',
-  backfaceVisibility: 'hidden',
-  transform: 'translateZ(0)',
-  transition: 'transform 0.25s ease-out',
-  '&:hover': {
-    background: 'linear-gradient(90deg,rgb(77,40,180),rgba(50,145,255,1))',
-    transform: 'translateY(-20px)',
-  },
+  display: "flex",
+  flexDirection: "column",
+  background: "linear-gradient(90deg,rgb(77,40,180),rgba(50,145,255,0.8))",
+  border: "1px solid grey",
+  borderRadius: "10px",
+  padding: "15px",
+  cursor: "pointer",
+  mozOsxFontSmoothing: "grayscale",
+  backfaceVisibility: "hidden",
+  transform: "translateZ(0)",
+  transition: "transform 0.25s ease-out",
+  "&:hover": {
+    background: "linear-gradient(90deg,rgb(77,40,180),rgba(50,145,255,1))",
+    transform: "translateY(-20px)"
+  }
 });
 
 const CardContainer = styled.div({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-around',
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-around"
 });
 
 const CardTitleContainer = styled.div({
-  color: 'rgb(209,203,237)',
+  color: "rgb(209,203,237)"
 });
 
 const CardImageContainer = styled.div({
-  margin: '0 0 0 80px',
+  margin: "0 0 0 80px"
 });
 
 const Card = props => (
--- src/components/Footer/footer.component.jsx
+++ src/components/Footer/footer.component.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import React from "react";
+import styled from "@emotion/styled";
 
 const FooterWrapper = styled.div({
-  height: '95px',
-  width: '100%',
-  background: 'rgb(29,26,55)',
-  backgroundPosition: 'bottom',
-  display: 'flex',
+  height: "75px",
+  width: "100%",
+  background: "rgb(29,26,55)",
+  backgroundPosition: "bottom",
+  display: "flex"
 });
 
 const CopyRight = styled.p({
-  color: 'white',
+  color: "rgb(132,129,160)"
 });
 
 const FooterContainer = styled.div({
-  top: '50%',
-  left: '50%',
-  display: 'flex',
-  alignItems: 'center',
-  alignContent: 'center',
-  margin: '0 auto',
+  top: "50%",
+  left: "50%",
+  display: "flex",
+  alignItems: "center",
+  alignContent: "center",
+  margin: "0 auto"
 });
 
 const Footer = () => (
