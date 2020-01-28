e15fa17a545b966637586980f75bf34e56905754
keshibat <kensukeshibata@gmail.com>
2020-01-28 15:28:48 +0900

    make App functional componet to class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/Home';
 import SignInPage from './pages/SignIn';
-import PrivateRoute from './components/routes/PrivateRoute';
 import Header from './components/Header/header.component';
 import Footer from './components/Footer/footer.component';
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <PrivateRoute exact path="/" component={HomePage} />
-        <Route path="/signin" component={SignInPage} />
-        <Route path="/free" component={HomePage} />
-        <PrivateRoute path="/sample" component={HomePage} />
-      </Switch>
-      <Footer />
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div>
+//       <Header />
+//       <Switch>
+//         <Route exact path="/" component={HomePage} />
+//         <Route path="/signin" component={SignInPage} />
+//       </Switch>
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/signin" component={SignInPage} />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
+}
+
 export default App;
--- src/components/SignIn/sign-in.component.jsx
+++ src/components/SignIn/sign-in.component.jsx
@@ -1,76 +1,61 @@
-import React from "react";
-import { withRouter } from "react-router";
-import styled from "@emotion/styled";
-import FormInput from "../FormInput/form-input.component";
-import CustomButton from "../CustomButton/custom-button.component";
-import ForgetPassword from "../ForgetPassword/forget-password.component";
-import auth from "../stores/auth";
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+import FormInput from '../FormInput/form-input.component';
+import CustomButton from '../CustomButton/custom-button.component';
+import ForgetPassword from '../ForgetPassword/forget-password.component';
 
 const SignInWrapper = styled.div({
-  width: "380px",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center"
+  width: '380px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
 });
 
 const AccountTitle = styled.h2({
-  color: "#2568b9",
-  margin: "10px 0"
+  color: '#2568b9',
+  margin: '10px 0',
 });
 
-class SignIn extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      email: "",
-      password: ""
-    };
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit = async event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    //TODO call auth API
-    auth.signin(() => {
-      this.props.history.push(this.props.location.state.from);
-    });
   };
 
-  handleInputChange = event => {
-    const { target } = event;
-    const { value } = target;
-    const { name } = target;
-    this.setState({ [name]: value });
+  const handleChangeEmail = e => {
+    setEmail(e.target.value);
+  };
+  const handleChangePassword = e => {
+    setPassword(e.target.value);
   };
 
-  render() {
-    return (
-      <SignInWrapper>
-        <AccountTitle>I already have an account</AccountTitle>
-        <form onSubmit={this.handleSubmit}>
-          <FormInput
-            name="email"
-            type="email"
-            handleChange={this.handleInputChange}
-            value={this.state.email}
-            label="email"
-            required
-          />
-          <FormInput
-            name="password"
-            type="password"
-            value={this.state.password}
-            handleChange={this.handleInputChange}
-            label="password"
-            required
-          />
-          <CustomButton type="submit"> Sign in </CustomButton>
-        </form>
-        <ForgetPassword />
-      </SignInWrapper>
-    );
-  }
-}
+  return (
+    <SignInWrapper>
+      <AccountTitle>I already have an account</AccountTitle>
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          name="email"
+          type="email"
+          handleChange={handleChangeEmail}
+          value={email}
+          label="email"
+          required
+        />
+        <FormInput
+          name="password"
+          type="password"
+          value={password}
+          handleChange={handleChangePassword}
+          label="password"
+          required
+        />
+        <CustomButton type="submit"> Sign in </CustomButton>
+      </form>
+      <ForgetPassword />
+    </SignInWrapper>
+  );
+};
 
-export default withRouter(SignIn);
+export default SignIn;
